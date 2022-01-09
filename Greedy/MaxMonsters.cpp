5b6bc4221f01d0cae66b701773f3287eaa32ff61
Md Owais Umar <owaisumar9@gmail.com>
2022-01-09 08:32:08 +0530

    Added

---
+++ Greedy/MaxMonsters.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int xp;
+    cin>>xp;
+    vector<int> P(n);
+    for(int i=0; i<n; i++)
+        cin>>P[i];
+    vector<int> B(n);
+    for(int i=0; i<n; i++)
+        cin>>B[i];
+    vector<pair<int, int>> PB(n);
+    for(int i=0; i<n; i++)
+    {
+        PB[i].first=P[i];
+        PB[i].second=B[i];
+    }
+    sort(PB.begin(), PB.end());
+    int count=0;
+    for(int i=0; i<n; i++)
+    {
+        if(PB[i].first<=xp)
+        {
+            xp+=PB[i].second;
+            count++;
+        }
+        else
+        {
+            cout<<count<<endl;
+            break;
+        }
+    }
+    return 0;
+}
